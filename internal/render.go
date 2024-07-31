e0a1e8735ec167880447c1a83df4f1a57c911ca1
Dwi Andhika <dwiandhikaaji@gmail.com>
2024-07-31 23:27:04 +0700

    feat(helpers): implement several util helpers
    - array of objects slicing, sorting, filtering
    - few json related util helpers

--- internal/render.go
+++ internal/render.go
@@ -2,6 +2,10 @@ package internal
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"sort"
 
 	"github.com/aymerick/raymond"
 	"github.com/yuin/goldmark"
@@ -9,6 +13,176 @@ import (
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/renderer/html"
 )
+
+func init() {
+	// create a custom helper for raymond for basic array methods, like reverse, sort, slice, etc.
+	raymond.RegisterHelper("slice", func(context []interface{}, start int, end int, options *raymond.Options) string {
+		result := options.Inverse()
+		if !raymond.IsTrue(context) {
+			return result
+		}
+
+		val := reflect.ValueOf(context)
+		if val.Kind() != reflect.Array && val.Kind() != reflect.Slice {
+			fmt.Println("WARNING: Context must be an array or slice")
+			return result
+		}
+
+		if end < 0 {
+			fmt.Println("WARNING: End index must be greater than or equal to 0")
+			return result
+		}
+
+		if start > end {
+			fmt.Println("WARNING: Start index must be less than or equal to end index")
+			return result
+		}
+
+		if start == end {
+			return result
+		}
+
+		if start >= val.Len() {
+			fmt.Println("WARNING: Start index must be less than the length of the array or slice")
+			return result
+		}
+
+		start = max(start, 0)
+		end = min(end, val.Len())
+
+		ctxSlice := context[start:end]
+
+		return options.FnWith(ctxSlice)
+	})
+
+	raymond.RegisterHelper("withSortByDate", func(context []interface{}, key string, order string, options *raymond.Options) string {
+		result := options.Inverse()
+		if !raymond.IsTrue(context) {
+			return result
+		}
+
+		val := reflect.ValueOf(context)
+		if val.Kind() != reflect.Array && val.Kind() != reflect.Slice {
+			fmt.Println("WARNING: Context must be an array or slice")
+			return result
+		}
+
+		if val.Len() == 0 {
+			return result
+		}
+
+		if order != "asc" && order != "desc" {
+			fmt.Println("WARNING: Order must be either 'asc' or 'desc'")
+			return result
+		}
+
+		if key == "" {
+			fmt.Println("WARNING: Key must be a non-empty string")
+			return result
+		}
+
+		sort.Slice(context, func(i, j int) bool {
+			iDate, ok := context[i].(map[string]interface{})[key].(string)
+			if !ok {
+				return false
+			}
+
+			jDate, ok := context[j].(map[string]interface{})[key].(string)
+			if !ok {
+				return false
+			}
+
+			if order == "desc" {
+				return iDate > jDate
+			}
+			return iDate < jDate
+		})
+
+		return options.FnWith(context)
+	})
+
+	raymond.RegisterHelper("withFilter", func(context []interface{}, key string, value interface{}, options *raymond.Options) string {
+		result := options.Inverse()
+		if !raymond.IsTrue(context) {
+			return result
+		}
+
+		val := reflect.ValueOf(context)
+		if val.Kind() != reflect.Array && val.Kind() != reflect.Slice {
+			fmt.Println("WARNING: Context must be an array or slice")
+			return result
+		}
+
+		if val.Len() == 0 {
+			return result
+		}
+
+		if key == "" {
+			fmt.Println("WARNING: Key must be a non-empty string")
+			return result
+		}
+
+		filtered := make([]interface{}, 0)
+		for _, item := range context {
+			itemMap, ok := item.(map[string]interface{})
+			if !ok {
+				continue
+			}
+
+			if itemMap[key] == value {
+				filtered = append(filtered, item)
+			}
+		}
+
+		return options.FnWith(filtered)
+	})
+
+	raymond.RegisterHelper("toJson", func(context interface{}, options *raymond.Options) string {
+		if context == nil {
+			return ""
+		}
+
+		b, err := json.Marshal(context)
+		if err != nil {
+			fmt.Println("WARNING: Failed to marshal context to JSON")
+			return ""
+		}
+
+		return string(b)
+	})
+
+	raymond.RegisterHelper("toJsonDiv", func(context interface{}, options *raymond.Options) raymond.SafeString {
+		if context == nil {
+			return ""
+		}
+
+		b, err := json.MarshalIndent(context, "", "\t")
+		if err != nil {
+			fmt.Println("WARNING: Failed to marshal context to pretty JSON")
+			return ""
+		}
+
+		result := fmt.Sprintf("<div style=\"white-space: pre-wrap;\">%s</div>", string(b))
+
+		return raymond.SafeString(result)
+	})
+
+	raymond.RegisterHelper("logJson", func(context interface{}, options *raymond.Options) string {
+		if context == nil {
+			return ""
+		}
+
+		b, err := json.MarshalIndent(context, "", "\t")
+		if err != nil {
+			fmt.Println("WARNING: Failed to marshal context to pretty JSON")
+			return ""
+		}
+
+		fmt.Println(string(b))
+
+		return ""
+	})
+}
 
 var md = goldmark.New(
 	goldmark.WithExtensions(
